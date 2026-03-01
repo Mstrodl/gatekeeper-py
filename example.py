50dc9195e96e729166b96ccf12b7fdb47ec71273
Mary Strodl <ipadlover8322@gmail.com>
2026-03-01 17:28:44 -0500

    Add example

---
+++ example.py
@@ -0,0 +1,19 @@
+import gatekeeper_py
+
+r=gatekeeper_py.Reader("pn532_uart:/dev/ttyACM0", gatekeeper_py.RealmType.MemberProjects)
+
+while True:
+  tag = r.poll_for_tag()
+  if tag:
+    print("OMG!!! TAG!!!")
+    print("Tag: " + str(tag.get_tag_type()))
+    try:
+      aid = tag.get_user()
+    except Exception as e:
+      print("oops: " + str(e))
+      continue
+    print("Association ID: " + aid)
+    user = r.fetch_user(aid)
+    user = user["user"]
+    print("Username: " + user["uid"])
+    print("Hello " + user["cn"])
